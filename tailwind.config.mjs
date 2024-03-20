6272baba782405fcbabca72c49561092cdec78c1
Daniel Castillo Guindos <danielcastilloguindos@gmail.com>
2024-03-20 18:51:52 +0100

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,100 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				// atomic: ["Atomic", "cursive"],
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				accent: "var(--color-accent)",
+				twitch: "var(--color-twitch)",
+				ice: "var(--color-twitch-ice)",
+			},
+			screens: {
+				xs: "360px",
+				...defaultTheme.screens,
+				"3xl": "1650px",
+			},
+			keyframes: {
+        lastSlideToLeft: {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '50%': {
+            transform: 'translateX(-50%)',
+          },
+          '99%': {
+            transform: 'translateX(-100%)',
+          },
+          '100%': {
+            display: 'none',
+          }
+        },
+        newSlideToLeft: {
+          '0%': {
+            transform: 'translateX(100%)',
+            display: 'block',
+          },
+          '50%': {
+            transform: 'translateX(50%)',
+          },
+          '100%': {
+            transform: 'translateX(0%)',
+          }
+        },
+        topbarx: {
+          '0%': {
+            transform: 'rotate(0deg)',
+            left: '25%'
+          },
+          '50%': {
+              transform: 'rotate(68,25deg)',
+              left: '25%',
+              top: '50%',
+          },
+          '100%': {
+              transform: 'rotate(135deg)',
+              left: '25%',
+              top: '50%',
+          }
+        },
+        middlebarx: {
+          '0%': {
+            width: 'auto'
+          },
+          '100%': {
+            width: '0'
+          }
+        },
+        bottombarx: {
+          '0%': {
+            transform: 'rotate(0deg)',
+            left: '25%',
+          },
+          '50%': {
+              transform: 'rotate(-68,25deg)',
+              left: '25%',
+              top: '50%',
+          },
+          '100%': {
+              transform: 'rotate(-135deg)',
+              left: '25%',
+              top: '50%',
+          }
+        }
+      },
+      animation: {
+        lastSlideToLeft: 'lastSlideToLeft .8s linear 1s',
+        newSlideToLeft: 'newSlideToLeft .8s linear 1s',
+        topbarx: 'topbarx .5s forwards',
+        middlebarx: 'middlebarx .0s forwards',
+        bottombarx: 'bottombarx .5s forwards',
+      },
+		},
+	},
+}
